a01a261024e95b2e897d33b1e50ea9be25542b67
Pablo López Fernández <145041522+pablolfz@users.noreply.github.com>
2025-10-31 21:26:23 +0100

    app.py

--- app.py
+++ app.py
@@ -126,4 +126,3 @@ if __name__ == '__main__':
     # Esta parte solo se ejecuta si corre el archivo directamente (local)
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
-
